ab52481504160e8bbc2a5f56d141bc68f11df36e
GappleCider <117851605+GappleCider@users.noreply.github.com>
2023-01-28 10:11:04 -0800

    Update polygon.js

--- src/polygons/polygon.js
+++ src/polygons/polygon.js
@@ -1,1 +1,35 @@
+export class Polygon {
+  constructor(x, y, width, height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.update();
 
+  }
+  generatePolygon() {
+    let points = [];
+
+    points.push({
+      x: this.x + (this.width / 2),
+      y: this.y + (this.height / 2)
+    })
+    points.push({
+      x: this.x - (this.width / 2),
+      y: this.y + (this.height / 2)
+    })
+    points.push({
+      x: this.x - (this.width / 2),
+      y: this.y - (this.height / 2)
+    })
+    points.push({
+      x: this.x + (this.width / 2),
+      y: this.y - (this.height / 2)
+    })
+
+    return points;
+  }
+  update() {
+    this.polygon = this.generatePolygon();
+  }
+}
